da8eecaf6ab52e1201c2e265154cacdcc6e2cf15
evertonschultz <evertonf.m.schultz98@gmail.com>
2021-08-10 19:10:05 -0300

    first commit

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,36 @@
+import '../styles/sidebar.scss';
+
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  genres: GenreResponseProps[];
+  clickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({genres, clickButton, selectedGenreId}: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => clickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
+}
